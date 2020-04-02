94d3014830b25668684db83d7129bba3e00c1678
Juliano Alves <von.juliano@gmail.com>
2020-04-02 14:19:00 +0100

    Datas podem retornar a String "Sem informação". Nós vamos retornar uma data no ano 1900 quando for o caso

--- src/main/scala/pdt/client/decoders.scala
+++ src/main/scala/pdt/client/decoders.scala
@@ -1,11 +1,16 @@
 package pdt.client
 
+import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 import io.circe.Decoder
 
+import scala.util.Try
+
 object decoders {
   private val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
 
-  implicit val localDateDecoder = Decoder.decodeLocalDateWithFormatter(formatter)
+  implicit val localDateDecoder = Decoder.decodeString.emapTry { str =>
+    Try(LocalDate.parse(str, formatter)).orElse(Try(LocalDate.of(1900, 1, 1)))
+  }
 }
