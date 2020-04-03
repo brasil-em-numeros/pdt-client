430440d07ce3628a6a96731c7d14c68d8a515a14
Juliano Alves <von.juliano@gmail.com>
2020-04-03 11:21:55 +0100

    Ajustando decoders. "decodePossiblyWrappedValue" é demais mesmo

--- src/main/scala/pdt/client/decoders.scala
+++ src/main/scala/pdt/client/decoders.scala
@@ -4,13 +4,27 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 import io.circe.Decoder
+import pdt.domain.AbrangenciaDefinidaDecisaoJudicial
 
 import scala.util.Try
 
 object decoders {
   private val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
 
-  implicit val localDateDecoder = Decoder.decodeString.emapTry { str =>
-    Try(LocalDate.parse(str, formatter)).orElse(Try(LocalDate.of(0, 1, 1)))
-  }
+  private val emptyLocalDateDecoder = Decoder.decodeString.map(_ => LocalDate.of(0, 1, 1))
+
+  implicit val localDateDecoder = Decoder.decodeLocalDateWithFormatter(formatter).or(emptyLocalDateDecoder)
+
+  implicit val bigDecimalDateDecoder =
+    Decoder.decodeString.emapTry { str =>
+      Try(BigDecimal(str.replace(".", "").replace(",", ".")))
+    }.or(Decoder.decodeBigDecimal)
+
+  implicit val decodeAbrangenciaDefinidaDecisaoJudicial =
+    decodePossiblyWrappedValue("descricao", AbrangenciaDefinidaDecisaoJudicial)
+
+  private def decodePossiblyWrappedValue[A](field: String, f: String => A): Decoder[A] =
+    Decoder.decodeJsonObject.map(_.apply(field))
+      .map { case Some(value) => f(value.toString) }
+      .or(Decoder.decodeJson.map(json => f(json.toString)))
 }
